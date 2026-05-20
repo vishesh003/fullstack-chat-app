d48343432bbd5d83c01f200c31c00ad0da34c474
vishesh003 <162690265+vishesh003@users.noreply.github.com>
2026-05-20 11:13:35 +0530

    fixed production deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-import "./lib/config.js"; 
+import "./lib/config.js";
 
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
@@ -12,50 +12,51 @@ import { app, server } from "./lib/socket.js";
 
 import path from "path";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 
-// ✅ DEFINE BEFORE USING
 const __dirname = path.resolve();
 
 app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(cors({
-  origin: process.env.NODE_ENV === "production"
-  ? "https://fullstack-chat-app-jy5r.onrender.com"
-  : "http://localhost:5173",
-}));
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://fullstack-chat-app-1-g6i1.onrender.com"
+        : "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-// Routes
+// API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // Production setup
 if (process.env.NODE_ENV === "production") {
 
-    app.use(
-        express.static(
-            path.join(__dirname, "frontend/dist")
-        )
+  app.use(
+    express.static(
+      path.join(__dirname, "frontend/dist")
+    )
+  );
+
+  app.get("/*", (req, res) => {
+
+    res.sendFile(
+      path.join(
+        __dirname,
+        "frontend",
+        "dist",
+        "index.html"
+      )
     );
-
-    app.get("*", (req, res) => {
-
-        res.sendFile(
-            path.join(
-                __dirname,
-                "frontend",
-                "dist",
-                "index.html"
-            )
-        );
-    });
+  });
 }
 
 server.listen(PORT, () => {
-
-    console.log("server is running on port:" + PORT);
-
-    connectDB();
+  console.log("Server running on port:", PORT);
+  connectDB();
 });
