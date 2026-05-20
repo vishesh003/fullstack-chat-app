65e2755e4344b78e63aafacdd834a4f5f8be4037
vishesh003 <162690265+vishesh003@users.noreply.github.com>
2026-05-20 12:23:25 +0530

    fixed auth rerender issue

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -11,17 +11,13 @@ export const generateToken = (userId, res) => {
   );
 
   res.cookie("jwt", token, {
-
     maxAge: 7 * 24 * 60 * 60 * 1000,
 
     httpOnly: true,
 
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
 
-    sameSite:
-      process.env.NODE_ENV === "production"
-        ? "none"
-        : "strict",
+    sameSite: "none",
   });
 
   return token;
